fb0e9e3a3723b19b9934b279ec4ccac8ef69f067
Connor Duncan <connorduncan1223@gmail.com>
2025-09-08 21:42:32 +0200

    Day 6: Section 7- error handling

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,8 @@
+using System.Security.Cryptography;
 using System.Text;
 using API.Data;
 using API.Interfaces;
+using API.Middleware;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -30,9 +32,11 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             ValidateAudience = false
         };
     });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>(); 
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
 
 app.UseAuthentication();
